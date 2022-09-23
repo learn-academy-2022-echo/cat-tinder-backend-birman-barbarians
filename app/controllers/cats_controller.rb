1ecb539baebf0b494240b994b08214d9c9b78b96
JPD99 <jeremedoan@gmail.com>
2022-09-23 10:40:38 -0700

    index, post, and patch completed

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,2 +1,29 @@
 class CatsController < ApplicationController
+
+    def index
+        cats = Cat.all
+        render json: cats
+    end
+  
+    def create
+        cat = Cat.create(cat_params)
+        render json:cat
+    end
+  
+    def update
+        cat = Cat.find(params[:id])
+        Cat.update(cat_params)
+        if cat.valid?
+            render json: cat
+        else 
+            render json: cat.errors, status: 422
+        end
+    end
+
 end
+
+private 
+def cat_params
+    params.require(:cat).permit(:name, :age, :enjoys, :image)
+  end
+
